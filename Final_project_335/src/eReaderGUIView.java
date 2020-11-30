11f711836177c3a9253a8885637a53785ea09da2
brandonsmith <brandonsmith@brandons-mbp>
2020-11-30 10:50:03 -0700

    Added buttons with images.

--- Final_project_335/src/eReaderGUIView.java
+++ Final_project_335/src/eReaderGUIView.java
@@ -23,13 +23,21 @@ import javafx.stage.Stage;
 public class eReaderGUIView  extends Application{
 	public static final int WIDTH  = 1072;
 	public static final int HEIGHT = 1448;
-	
+	public static final int BUTTON_DIM = 50;
 	// OBJECTS
 	private eReaderController controller;
 	private BorderPane        window;
 	private eReaderModel      model; 
-	private GridPane          gridpane;
+	private GridPane          gridPane;
+	private GridPane   		  text;
 	private VBox toolbarVbox;
+	private Button homeButton;
+	private Button backButton;
+	private Button forwardButton;
+	private Button settingsButton;
+	private Button searchButton;
+	
+	
 	/**
 	 * Purpose:
 	 */
@@ -37,7 +45,13 @@ public class eReaderGUIView  extends Application{
 		this.window     = new BorderPane  (); 
 		this.model      = new eReaderModel();
 		this.controller = new eReaderController(model);
-		this.gridpane = new GridPane();
+		this.gridPane   = new GridPane();
+		//this.homeButton = new Button("Home");
+		this.backButton = new Button("Back");
+		this.forwardButton  = new Button("Forward");
+		this.settingsButton = new Button("Settings");
+		this.toolbarVbox    = new VBox();
+		this.text = new GridPane(); // Holds page info
 	}
 	
 	/**
@@ -47,36 +61,78 @@ public class eReaderGUIView  extends Application{
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("E-Reader");
 		
-	
-		VBox toolbarVbox = new VBox();
 		
 		
-		Button homeButton     = new Button("Home"); // Get Images for these and add later
-		Button backButton     = new Button("Back");
-		Button forwardButton  = new Button("Forward");
-		Button settingsButton = new Button("Settings");
-	
-		HBox toolbarHbox = new HBox(homeButton, backButton, forwardButton, settingsButton);
+		FileInputStream input1 = new FileInputStream("button_images/homeButton.png"    );
+		FileInputStream input2 = new FileInputStream("button_images/forwardButton.png" );
+		FileInputStream input3 = new FileInputStream("button_images/backButton.png"    );
+		FileInputStream input4 = new FileInputStream("button_images/settingsButton.png");
+		FileInputStream input5 = new FileInputStream("button_images/searchButton.png"  );
+		
+		Image image1 = new Image(input1);
+		Image image2 = new Image(input2);
+		Image image3 = new Image(input3);
+		Image image4 = new Image(input4);
+		Image image5 = new Image(input5);
+		
+        ImageView imageView1 = new ImageView(image1);
+        ImageView imageView2 = new ImageView(image2);
+        ImageView imageView3 = new ImageView(image3);
+        ImageView imageView4 = new ImageView(image4);
+        ImageView imageView5 = new ImageView(image5);
+       
+        
+        imageView1.setFitWidth (BUTTON_DIM);
+        imageView1.setFitHeight(BUTTON_DIM);
+        
+        imageView2.setFitWidth (BUTTON_DIM);
+        imageView2.setFitHeight(BUTTON_DIM);
+        
+        imageView3.setFitWidth (BUTTON_DIM);
+        imageView3.setFitHeight(BUTTON_DIM);
+        
+        imageView4.setFitWidth (BUTTON_DIM);
+        imageView4.setFitHeight(BUTTON_DIM);
+        
+        imageView5.setFitWidth (BUTTON_DIM);
+        imageView5.setFitHeight(BUTTON_DIM);
+        
+        this.homeButton     = new Button("", imageView1);
+        this.forwardButton  = new Button("", imageView2);
+        this.backButton     = new Button("", imageView3);
+		this.settingsButton = new Button("", imageView4);
+		this.searchButton   = new Button("", imageView5);
+		
+		HBox toolbarHbox = new HBox(this.homeButton, this.forwardButton, this.backButton, 
+				                    this.settingsButton, this.searchButton);
 		
 		
-		toolbarHbox.setSpacing(10);
+		toolbarHbox.setSpacing(100);
 		
 		toolbarVbox.getChildren().add(toolbarHbox);
-		this.gridpane.add(toolbarVbox, 0, 0); // Add to gridpane
+		this.gridPane.add(toolbarVbox, 0, 0); // Add to gridpane
+		this.window.setTop(this.gridPane); // Set gridpane to top
+		
+		
+		
+        
+		
+		
+		
 		
 		// Events for buttons
-		homeButton.setOnAction    (e-> { System.out.println("Home Button");});
-		backButton.setOnAction    (e-> { System.out.println("Back"       );});
-		forwardButton.setOnAction (e-> { System.out.println("Forward"    );});
-		settingsButton.setOnAction(e-> { System.out.println("Settings"   );});
+		this.homeButton.setOnAction    (e-> { System.out.println("Home Button");});
+		this.backButton.setOnAction    (e-> { System.out.println("Back"       );});
+		this.forwardButton.setOnAction (e-> { System.out.println("Forward"    );});
+		this.settingsButton.setOnAction(e-> { System.out.println("Settings"   );});
+		this.searchButton.setOnAction  (e-> { System.out.println("Search"     );});
 		
 		
 		
 		
 		
 		
-		
-		Scene scene = new Scene(this.gridpane, WIDTH, HEIGHT);
+		Scene scene = new Scene(this.window, WIDTH, HEIGHT);
 		stage.setScene(scene);
 
 		stage.show();  // Show the stage 
